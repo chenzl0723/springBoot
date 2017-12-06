6adba8c52f86a6ca16c09074223dfcf5de10e239
chenzaolong <zaolong.chen@hand-china.com>
2017-12-06 19:55:03 +0800

    [ADD]redis事务管理

---
+++ messaging-redis/src/main/java/com/chenzl/dao/RedisDao.java
@@ -0,0 +1,28 @@
+package com.chenzl.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by chenzl on 2017/12/6.
+ */
+@Repository
+public class RedisDao {
+
+    @Autowired
+    private StringRedisTemplate template;
+
+    public  void setKey(String key,String value){
+        ValueOperations<String, String> ops = template.opsForValue();
+        ops.set(key,value,1, TimeUnit.MINUTES);//1分钟过期
+    }
+
+    public String getValue(String key){
+        ValueOperations<String, String> ops = this.template.opsForValue();
+        return ops.get(key);
+    }
+}
---
+++ messaging-redis/src/main/java/com/chenzl/message/Receiver.java
@@ -0,0 +1,26 @@
+package com.chenzl.message;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by chenzl on 2017/12/6.
+ */
+public class Receiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch;
+
+    @Autowired
+    public Receiver(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void receiveMessage(String message) {
+        LOGGER.info("Received <" + message + ">");
+        latch.countDown();
+    }
+}
