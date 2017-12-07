149f012a2f8c1f88875e0352c22d46f664b9c387
chenzaolong <zaolong.chen@hand-china.com>
2017-12-07 17:05:05 +0800

    [ADD]时间调度任务，定时任务

---
+++ schedulingtasks/src/main/java/com/chenzl/SchedulingtasksApplication.java
@@ -0,0 +1,14 @@
+package com.chenzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SchedulingtasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingtasksApplication.class, args);
+	}
+}
