61ff59b3f66465c1ad4d8c4feb0440c16080aef5
chenzaolong <zaolong.chen@hand-china.com>
2017-12-07 17:32:20 +0800

    [ADD]表单验证

---
+++ spbtvalidator/src/main/java/com/chenzl/SpbtvalidatorApplication.java
@@ -0,0 +1,12 @@
+package com.chenzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtvalidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbtvalidatorApplication.class, args);
+	}
+}
---
+++ spbtvalidator/src/main/java/com/chenzl/web/WebController.java
@@ -0,0 +1,37 @@
+package com.chenzl.web;
+
+import com.chenzl.entity.PersonForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.validation.Valid;
+
+/**
+ * Created by chenzl on 2017/12/7.
+ */
+@Controller
+public class WebController extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+
+    @GetMapping("/")
+    public String showForm(PersonForm personForm) {
+        return "form";
+    }
+
+    @PostMapping("/")
+    public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
+
+        return "redirect:/results";
+    }
+}
